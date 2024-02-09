e4d46cadadbb0287da0ee799e5bb1d83fc813b42
Azhar-Vlad <vladazhar2@gmail.com>
2024-02-09 16:06:41 +0300

    task1

---
+++ tasks3.js
@@ -0,0 +1,14 @@
+const numbers = [1,1,1,1,1,2,2,2,2,2,3,4,4,4,5,5,6,6,7,7];
+function findOne(one) {
+    const count = {};
+    for (let num of one) {
+        count[num] = (count[num] || 0) + 1;
+    }
+ 
+    for (let num in count) {
+        if (count[num] === 1) {
+            return parseInt(num);
+        }
+    }
+}
+console.log(findOne(numbers))
